0c0755b0db9fb191b7f1218c49e9a828dd643ee0
Midichlorian007 <jambolich@mail.ru>
2023-03-26 00:29:20 +0600

    profile html

--- server/web/assets/app.js
+++ server/web/assets/app.js
@@ -1,1 +1,31 @@
 console.log("Connected")
+
+$("#floatingSelect").change(function () {
+
+    if (this.value === "Golang")
+        $("#minLvl").text("Middle"),
+            $("#maxLvl").text("Senior"),
+            $("#progresPercent").text("80%").width("80%");
+    else if (this.value === "Python")
+        $("#minLvl").text("Junior"),
+            $("#maxLvl").text("Middle"),
+            $("#progresPercent").text("90%").width("90%");
+    else if (this.value === "Java")
+        $("#minLvl").text("Junior"),
+            $("#maxLvl").text("Middle"),
+            $("#progresPercent").text("10%").width("10%");
+    else
+        $("#minLvl").text("Junior"),
+            $("#maxLvl").text("Middle"),
+            $("#progresPercent").text("50%").width("50%");
+
+
+});
+
+$("#btnFindJob").click(function(){
+    $(this).text($(this).text() == 'В поиске работы' ? 'Уже работаю' : 'В поиске работы');
+    $("#jobPlace").toggleClass('d-none')
+    $("#jobFunc").toggleClass('d-none')
+});
+
+
